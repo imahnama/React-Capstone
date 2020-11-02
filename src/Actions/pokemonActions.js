96c669933a16e604e063a3c2cb62602f76ba03f5
imahnama <muannama25@gmail.com>
2020-11-02 12:19:56 +0300

    Add pokemon actions

---
+++ src/Actions/pokemonActions.js
@@ -0,0 +1,44 @@
+/* eslint-disable  import/prefer-default-export */
+/* eslint-disable  no-unused-vars */
+import axios from 'axios';
+
+export const GetPokemonList = page => async dispatch => {
+  try {
+    dispatch({
+      type: 'POKEMON_LIST_LOADING',
+    });
+
+    const perPage = 15;
+
+    const res = await axios.get(`https://pokeapi.co/api/v2/pokemon?limit=${perPage}`);
+
+    dispatch({
+      type: 'POKEMON_LIST_SUCCESS',
+      payload: res.data,
+    });
+  } catch (e) {
+    dispatch({
+      type: 'POKEMON_LIST_FAIL',
+    });
+  }
+};
+
+export const GetPokemon = pokemon => async dispatch => {
+  try {
+    dispatch({
+      type: 'POKEMON_MULTIPLE_LOADING',
+    });
+
+    const res = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+
+    dispatch({
+      type: 'POKEMON_MULTIPLE_SUCCESS',
+      payload: res.data,
+      pokemonName: pokemon,
+    });
+  } catch (e) {
+    dispatch({
+      type: 'POKEMON_MULTIPLE_FAIL',
+    });
+  }
+};
