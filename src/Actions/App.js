c4335c386778233416dc5bcd2325f2b2f397e859
imahnama <muannama25@gmail.com>
2020-11-02 12:19:24 +0300

    add NavLink component and export function

---
+++ src/Actions/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {
+  Switch, Route, NavLink, Redirect,
+} from 'react-router-dom';
+import PokemonList from '../Containers/PokemonList';
+import Pokemon from '../Containers/Pokemon';
+
+function App() {
+  return (
+    <div className="app">
+      <nav className="nav-content d-flex">
+        <NavLink to="/">Search</NavLink>
+      </nav>
+      <Switch>
+        <Route path="/" exact component={PokemonList} />
+        <Route path="/pokemon/:pokemon" exact component={Pokemon} />
+        <Redirect to="/" />
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
