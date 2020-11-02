50be7c4b491dfaae905ff1039f12f426e00859f9
imahnama <muannama25@gmail.com>
2020-11-02 12:21:00 +0300

    get pokemon data

---
+++ src/Containers/Pokemon.js
@@ -0,0 +1,67 @@
+/* eslint-disable react/destructuring-assignment */
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/jsx-key */
+
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import _ from 'lodash';
+import { GetPokemon } from '../Actions/pokemonActions';
+
+const Pokemon = props => {
+  const pokemonName = props.match.params.pokemon;
+  const dispatch = useDispatch();
+  const pokemonState = useSelector(state => state.Pokemon);
+  React.useEffect(() => {
+    dispatch(GetPokemon(pokemonName));
+  }, []);
+
+  const ShowData = () => {
+    if (!_.isEmpty(pokemonState.data[pokemonName])) {
+      const pokeData = pokemonState.data[pokemonName];
+      return (
+        <div className="pokemon-wrapper">
+          <div className="item">
+            <h1>Sprites</h1>
+            <img src={pokeData.sprites.front_default} alt="" />
+            <img src={pokeData.sprites.back_default} alt="" />
+            <img src={pokeData.sprites.front_shiny} alt="" />
+            <img src={pokeData.sprites.back_shiny} alt="" />
+          </div>
+          <div className="item">
+            <h1>Stats</h1>
+            {pokeData.stats.map(el => (
+              <p>
+                {el.stat.name}
+                {' '}
+                {el.base_stat}
+              </p>
+            ))}
+          </div>
+          <div className="item">
+            <h1>Abilities</h1>
+            {pokeData.abilities.map(el => <p>{el.ability.name}</p>)}
+          </div>
+        </div>
+      );
+    }
+
+    if (pokemonState.loading) {
+      return <p>Loading...</p>;
+    }
+
+    if (pokemonState.errorMsg !== '') {
+      return <p>{pokemonState.errorMsg}</p>;
+    }
+
+    return <p>error getting pokemon</p>;
+  };
+  return (
+    <div className="card d-flex">
+      <h1>{pokemonName}</h1>
+      {ShowData()}
+    </div>
+  );
+};
+
+export default Pokemon;
