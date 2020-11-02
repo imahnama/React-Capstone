f2a3f0977c50fceeca355802bfd2f620ff4af582
imahnama <muannama25@gmail.com>
2020-11-02 12:22:54 +0300

    import files and set base for the project

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import Store from './Store';
+import App from './Actions/App';
+import './index.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
 ReactDOM.render(
-  <Provider store={store}>
   <React.StrictMode>
-  <App />
-  <React.StrictMode>
-  ,
-  </provider>,
+    <Provider store={Store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>,
   document.getElementById('root'),
 );
